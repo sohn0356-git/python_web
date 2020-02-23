a4153776a039cd91db18750d312f6507872361d0
sohn0356-git <sohn0356@gmail.com>
2020-02-23 23:16:24 +0900

    3장

---
+++ fcdjango_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\sohn0\desktop\myfloder\20_1\python_web\fcdjango_venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
