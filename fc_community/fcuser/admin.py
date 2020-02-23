0c57c6b37f7ae27719f04f7c814047258962aa71
sohn0356-git <sohn0356@gmail.com>
2020-02-24 08:44:40 +0900

    3-9

--- fc_community/fcuser/admin.py
+++ fc_community/fcuser/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
 
+from django.contrib import admin
+from .models import Fcuser
 # Register your models here.
+
+class FcuserAdmin(admin.ModelAdmin):
+    list_display = ('username','password')      #field들이 listing됨
+
+admin.site.register(Fcuser,FcuserAdmin)
--- fc_community/fcuser/models.py
+++ fc_community/fcuser/models.py
@@ -9,5 +9,10 @@ class Fcuser(models.Model):
                                 verbose_name = '비밀번호')
     registered_dttm = models.DateTimeField(auto_now_add=True,
                                             verbose_name='등록시간')
+
+    def __str__(self):              #username으로 화면에 띄우고 싶을 때
+        return self.username
     class Meta:
         db_table = 'fastcampus_fcuser'  #table_name을 직접 설정하고 싶을 때
+        verbose_name = 'fastcampus user'    #복수형이 들어감
+        #verbose_name_plural = 'fastcampus user'    #그냥 들어감
