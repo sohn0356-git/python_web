b2189916a8ba316863b47d7364e79245d7846d9b
sohn0356-git <sohn0356@gmail.com>
2020-02-24 08:27:08 +0900

    3-7

--- fc_community/fcuser/models.py
+++ fc_community/fcuser/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Fcuser(models.Model):
-    username = models.CharField(max_length = 64,
+    username = models.CharField(max_length = 32,
                                 verbose_name = '사용자명')
     password = models.CharField(max_length= 64,
                                 verbose_name = '비밀번호')
