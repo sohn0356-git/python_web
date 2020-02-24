a9909d14e83294d1e9ff02c6f32966d749abcd32
sohn0356-git <sohn0356@gmail.com>
2020-02-24 09:05:33 +0900

    3-11

--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def register(request):
-    return render(request,'register.html')
+    if request.method == 'GET':
+        return render(request,'register.html')
+    else:
+        
+        return render(requset,'register.html')
