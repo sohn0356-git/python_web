5e2141c454656e82afad6fc33f67547ec5a83b4e
sohn0356 <sohn0356@gmail.com>
2020-02-25 11:12:34 +0900

    3-23

--- fc_community/board/urls.py
+++ fc_community/board/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views 
 urlpatterns = [
-    path('list/', views.board_list)
+    path('list/', views.board_list),
+    path('write/', views.board_write),
+    path('detail/<int:pk>',views.board_detail)
 ]
--- fc_community/board/views.py
+++ fc_community/board/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import BoardForm
 from .models import Board
+from fcuser.models import Fcuser
 
 # Create your views here.
 
@@ -8,3 +10,27 @@ def board_list(request):
     boards = Board.objects.all().order_by('-id')
     
     return render(request,'board_list.html', {'boards':boards})
+
+def board_write(request):
+    if request.method=='POST':
+        form = BoardForm(request.POST)
+        if form.is_valid():
+            user_id = request.session.get('user')
+            fcuser = Fcuser.objects.get(pk=user_id)
+
+            board = Board()
+            board.title = form.cleaned_data['title']
+            board.contents = form.cleaned_data['contents']
+            board.writer = fcuser
+            board.save()
+
+            return redirect('/board/list/')
+    else:
+        form = BoardForm()
+
+    return render(request,'board_write.html',{'form':form})
+
+def board_detail(request, pk):
+    board = Board.objects.get(pk=pk)
+
+    return render(request, 'board_detail.html',{"board":board})
