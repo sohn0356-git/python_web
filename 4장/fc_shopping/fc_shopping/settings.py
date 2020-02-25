8ed11ec174bc12bb5c9a8f86a2ead21d42bccc74
sohn0356 <sohn0356@gmail.com>
2020-02-25 18:07:16 +0900

    4-3

--- 4장/fc_shopping/fc_shopping/settings.py
+++ 4장/fc_shopping/fc_shopping/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fcorder',
+    'fcproduct',
+    'fcuser',
 ]
 
 MIDDLEWARE = [
--- 4장/fc_shopping/fcproduct/models.py
+++ 4장/fc_shopping/fcproduct/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Fcproduct(models.Model):
+    name = models.CharField(max_length=256,verbose_name="상품명")
+    price = models.IntegerField(verbose_name="상품가격")
+    description = models.TextField(verbose_name="상품설명")
+    stock = models.IntegerField(verbose_name = "재고")
+    register_dttm = models.DateTimeField(auto_now_add=True, verbose_name="등록날짜")
+
+    class Meta:
+        db_table = 'fc_product'
+        verbose_name = "상품"
+        verbose_name_plural = "상품"
